4956c3f8c53873f0f7a0db12dc58a289a949b69c
Salman Hashmi <shash222@yahoo.com>
2020-03-02 13:26:37 -0500

    Refactored code, got API fetch and results display working

--- client/src/Views/Home.js
+++ client/src/Views/Home.js
@@ -25,29 +25,32 @@ export default class Home extends Component {
             "moreAboutInstructorLink": "",
             "instructorImageLocation": ""
         },
+        isLoading: true,
+
     };
 
-    componentDidMount() {
-        axios.post("https://server.durbah.org:5000/upcomingSeminarData").then((res) => {
-            console.log(res);
-            this.setState({ seminarData: res })
+    async getSeminarData() {
+        await axios.get("https://server.durbah.org:5000/upcomingSeminarData").then((res) => {
+            this.setState({ seminarData: res.data, isLoading: false })
         })
-        // this.setState({seminarData: seminarData})
     }
 
 
     render() {
+        this.getSeminarData();
         return (
-            <div className="container">
-                <Img id="homeImage" src={homeImg} />
-                <div className="countdownContainer" id="homeCountdownContainer">
-                    <div className="countdownHeading">
-                        <p className="countdownUntil" id="countdownUntilSeminar">COUNTDOWN UNTIL</p>
-                        <p className="countdownItem">{this.state.seminarData.seminarName.toUpperCase()}</p>
-                    </div>
-                    <Countdown datePassedMessage={["CLASS IS NOW ", <span key="liveTextSpan" className="liveText">IN SESSION</span>]} endDate={new Date(this.state.seminarData.seminarDate + " " + this.state.seminarData.seminarTime)} />
-                </div>
-            </div>
+            !this.state.isLoading
+                ? (<div className="container">
+                        <Img id="homeImage" src={homeImg} />
+                        <div className="countdownContainer" id="homeCountdownContainer">
+                            <div className="countdownHeading">
+                                <p className="countdownUntil" id="countdownUntilSeminar">COUNTDOWN UNTIL</p>
+                                <p className="countdownItem">{this.state.seminarData.seminarName.toUpperCase()}</p>
+                            </div>
+                            <Countdown datePassedMessage={["CLASS IS NOW ", <span key="liveTextSpan" className="liveText">IN SESSION</span>]} endDate={new Date(this.state.seminarData.seminarDate + " " + this.state.seminarData.seminarTime)} />
+                        </div>
+                    </div>)
+                : null
         );
     }
 }
