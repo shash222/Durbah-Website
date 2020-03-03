4b656bc25ae021e50153c5cbeb1095aa3255224a
Salman Hashmi <shash222@yahoo.com>
2020-03-03 17:34:27 -0500

    Added Admin Portal to Update Upcoming  Seminar Data Easily

---
+++ admin.durbah/javascript/index.js
@@ -0,0 +1,32 @@
+function updateConstantsFile() {
+    var inputs = document.getElementsByClassName("formInput");
+    if (inputs.length === 0) console.log("Nothing entered");
+    var updateObj = {};
+    for (var i = 0; i < inputs.length; i++) {
+        var input = inputs[i];
+        var inputValue = input.value.trim();
+        if (inputValue.length > 0) {
+            if (input.name === "promoVideoUrl") {
+                inputValue = getYoutubeEmbedCodeFromVideoUrl(input.value);
+            }
+            updateObj[input.name] = inputValue;
+        }
+    }
+    console.log(updateObj);
+    fetch("https://server.durbah.org:5000/upcomingSeminarDataPost", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(updateObj) // body data type must match "Content-Type" header
+
+    }).then((response) => {
+        console.log(response);
+    })
+}
+
+function getYoutubeEmbedCodeFromVideoUrl(url) {
+    var splitUrl = url.split("/");
+    var baseEmbedUrl = "https://www.youtube.com/embed/"
+    return baseEmbedUrl + splitUrl[splitUrl.length - 1]
+}
--- server/API.js
+++ server/API.js
@@ -4,6 +4,8 @@ const port = 5000;
 const fs = require('fs');
 const https = require('https');
 
+const UPCOMING_SEMINAR_DATA_JSON_FILE_PATH = "../constants/upcomingSeminarData.json";
+
 const httpsOptions = {
         cert: fs.readFileSync("/etc/letsencrypt/live/hashmi.site/fullchain.pem"),
         key: fs.readFileSync("/etc/letsencrypt/live/hashmi.site/privkey.pem")
@@ -11,8 +13,9 @@ const httpsOptions = {
 
 https.createServer(httpsOptions, app).listen(port);
 
-var upcomingSeminarData = JSON.parse(fs.readFileSync("../constants/upcomingSeminarData.json"));
+var upcomingSeminarData = JSON.parse(fs.readFileSync(UPCOMING_SEMINAR_DATA_JSON_FILE_PATH));
 
 app.get("/upcomingSeminarData", (req, res) => {
     res.send(upcomingSeminarData);
 })
+
